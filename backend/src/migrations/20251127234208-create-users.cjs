510f85c82184b90f07f849c78913a443774fefb5
Letz007 <leuxavieraraujo007@gmail.com>
2025-11-27 20:55:46 -0300

    feat: add Users migration and fix ES module issues for Sequelize

---
+++ backend/src/migrations/20251127234208-create-users.cjs
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('gen_random_uuid()'),
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      avatar_url: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  }
+};
+
+
